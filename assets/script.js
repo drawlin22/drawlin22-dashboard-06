50f4ccf4bbd115182552f6fecdb6d806c341503c
Donnie Rawlings <tacobellboi@yahoo.com>
2023-07-24 17:36:41 -0700

    added notes to JS

--- assets/script.js
+++ assets/script.js
@@ -12,7 +12,7 @@ function todaysWeather() {
     cityInputLocation =cityInput.value.trim(); /* added */
     fetchUrl = "https://api.openweathermap.org/data/2.5/weather?q="+cityInputLocation+"&APPID=093ae3ae21bb27eb5d4488dfe5718811&units=imperial" /* added */
 
-    fetch (fetchUrl)
+    fetch (fetchUrl) /* pulls the input entered by the user into the URL to load the appropriate data */
 
 .then(function (response) {
     return response.json();
@@ -34,7 +34,7 @@ imgSrc = data.weather[0].icon
 iconUrl = "http://openweathermap.org/img/w/"+imgSrc + ".png";
 img.src = iconUrl
 
-displayBox.append(cityEl)
+displayBox.append(cityEl) /* appending elements to the main display box */
 displayBox.append(img)
 displayBox.append(temperature)
 displayBox.append(wind)
@@ -46,7 +46,7 @@ function fiveDayForecast() {
   cityInputLocation =cityInput.value.trim(); /* added */
   fetchUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+cityInputLocation+"&APPID=093ae3ae21bb27eb5d4488dfe5718811&units=imperial" /* added */ 
   
-  fetch (fetchUrl)
+  fetch (fetchUrl) /* changed URL to grab future data for 5 day forcast */
     
     .then(function (response) {
         return response.json();
@@ -59,7 +59,7 @@ function fiveDayForecast() {
                 console.log(data.list[i])
        
 
-        let cardSection = document.createElement('section')
+        let cardSection = document.createElement('section') /* Create 5 day forcast cards */
         cardSection.className = "col-1 card card-body"
 
         let fiveDayDate = document.createElement('h5')
@@ -73,10 +73,10 @@ function fiveDayForecast() {
         wind.textContent = "Wind " + data.list[i].wind.speed + " MPH";
         humidity.textContent = "Humidity " + data.list[i].main.humidity + " %";
         imgSrc = data.list[i].weather[0].icon
-        iconUrl = "http://openweathermap.org/img/w/"+imgSrc + ".png";
+        iconUrl = "http://openweathermap.org/img/w/"+imgSrc + ".png"; /* adding URL to data object for icon */
         img.src = iconUrl
         
-        cardSection.appendChild(fiveDayDate)
+        cardSection.appendChild(fiveDayDate) /* appending 5 day forcast to the card section */
         cardSection.appendChild(img)
         cardSection.appendChild(temperature)
         cardSection.appendChild(wind)
@@ -90,8 +90,8 @@ function fiveDayForecast() {
     }
 
 button.addEventListener("click", function() {
-  displayBox.textContent = "";
-  prior.textContent = "";
+  displayBox.textContent = ""; /* clears prior search information */
+  prior.textContent = ""; /* clears prior search information */
    todaysWeather()
    fiveDayForecast()
    saveSearch()
@@ -100,8 +100,8 @@ button.addEventListener("click", function() {
 cityInput.addEventListener("keydown", function(event) {
   if(event.key ==='Enter'){
     event.preventDefault()
-    displayBox.textContent = "";
-    prior.textContent = "";
+    displayBox.textContent = ""; /* clears prior search information */
+    prior.textContent = ""; /* clears prior search information */
      todaysWeather()
      fiveDayForecast()
      saveSearch()
@@ -113,7 +113,7 @@ function saveSearch() {
 let searchLocation = JSON.parse(localStorage.getItem("cityInputLocation")) || [];
 let place = document.getElementById('input').value;
 
-searchLocation.push(place);
+searchLocation.push(place); /* pushing search inputs into the search location array */
 
 localStorage.setItem("cityInputLocation", JSON.stringify(searchLocation))
 
@@ -124,8 +124,8 @@ searchButton.textContent = place;
 searchButton.addEventListener("click", function(event) {
   cityInput.value = event.target.textContent
 
-  displayBox.textContent = "";
-  prior.textContent = "";
+  displayBox.textContent = ""; /* clears prior search information */
+  prior.textContent = "";/* clears prior search information */
    todaysWeather()
    fiveDayForecast()
 })
